8581de621ffd1e955905920dd929b5cccdcada29
AaronBison <aaronbison97@gmail.com>
2019-11-07 00:17:25 +0200

    DEMO

--- frontend/src/components/fittings/ExchangeCardDialog.vue
+++ frontend/src/components/fittings/ExchangeCardDialog.vue
@@ -3,15 +3,25 @@
     <template v-slot:activator="{ on }">
       <v-btn v-on="on" outlined color="success" text>Add</v-btn>
     </template>
-    <v-card>
-      <v-card-title primary-title>Exchange Card Dialog</v-card-title>
+    <v-card :loading="loading" class="px-5">
+      <v-card-title>Uploading {{currency}}</v-card-title>
 
-      <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
+      <v-text-field class="mx-5 mb-5" label="Amount to upload" clearable counter="7" />
+      <v-text-field
+        class="mx-5 mb-5"
+        label="Password"
+        :append-icon="show ? 'fas fa-eye' : 'fas fa-eye-slash'"
+        :type="show ? 'text' : 'password'"
+        counter="24"
+        @click:append="show = !show"
+        :rules="[rules.required, rules.min]"
+      />
 
       <v-divider></v-divider>
 
       <v-card-actions>
         <v-spacer></v-spacer>
+        <v-btn v-on="on" @click="adding" color="success" text>Add</v-btn>
         <v-btn text @click="dialog = false">back</v-btn>
       </v-card-actions>
     </v-card>
@@ -20,10 +30,23 @@
 <script>
 export default {
   name: "ExchangeDialog",
+  props: ["currency"],
   data() {
     return {
-      dialog: false
+      show: false,
+      dialog: false,
+      loading: false,
+      rules: {
+        required: value => !!value || "Password is equired",
+        min: v => v.length >= 8 || "Min 8 characters"
+      }
     };
+  },
+  methods: {
+    adding() {
+      this.loading = 'success';
+      setTimeout(() => (this.loading = false, this.dialog = false), 2000);
+    }
   }
 };
 </script>
