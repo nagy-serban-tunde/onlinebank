bae3cc558c650036c0dd222d17886f9268f3964b
AaronBison <aaronbison97@gmail.com>
2019-11-07 00:17:18 +0200

    DEMO

--- frontend/src/components/Account.vue
+++ frontend/src/components/Account.vue
@@ -1,19 +1,55 @@
 <template>
-    <v-app>
-        <data-sheet :dataSheet="dataSheet"/>
-    </v-app>
+  <div>
+    <v-card dark height="60">
+      <v-card-title primary-title class="justify-center">
+        <v-subheader class="subtitle-0">Personal data</v-subheader>
+      </v-card-title>
+    </v-card>
+    <v-list-item>
+      <v-list-item-content>
+        <v-row dense>
+          <v-col>
+            <v-card dark outlined max-width="300">
+              <v-layout center>
+                <v-subheader class="subtitle-1">Name:</v-subheader>
+                <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+              </v-layout>
+            </v-card>
+            <v-card dark outlined max-width="300">
+              <v-subheader class="subtitle-2">Birth date:</v-subheader>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card dark outlined max-width="300">
+              <v-subheader class="subtitle-3">E-mail:</v-subheader>
+            </v-card>
+            <v-card dark outlined max-width="300">
+              <v-subheader class="subtitle-4">Summa:</v-subheader>
+              <v-layout mr-5 justify-space-around>
+                <v-card-actions color="green">
+                  <v-btn outlined color="success" text>Add</v-btn>
+                </v-card-actions>
+              </v-layout>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-card dark outlined max-width="300">
+          <v-subheader class="subtitle-5">Picture:</v-subheader>
+          <v-layout>
+            <img width="200px" src="https://randomuser.me/api/portraits/women/72.jpg" />
+          </v-layout>
+        </v-card>
+      </v-list-item-content>
+    </v-list-item>
+  </div>
 </template>
 
 <script>
 
-import DataSheet from "./fittings/DataSheet";
-
 export default {
-    name: "Account",
-    components: {DataSheet},
-}
+  name: "Account",
+};
 </script>
 
 <style scoped>
-
 </style>
