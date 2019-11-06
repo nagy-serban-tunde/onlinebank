f0b5674c21effa75f4698ae856eab16eec9f0582
Tunde <nagyserbantunde@gmail.com>
2019-11-06 20:00:56 +0200

    Json sorting

--- datacollector/WebScraping.py
+++ datacollector/WebScraping.py
@@ -10,7 +10,9 @@ class WebScraping(object):
         self.webpages = webpages
         self.webpages_name = webpages_name
         self.List_elements = []
-        self.List_valutas  = ['RON','GBP', 'USD']
+        self.List_EUROtoRON_GBP_USD = []
+        self.List_GBPtoRON_EURO_USD = []
+        self.List_USDtoRON_EURO_GBP = []
         self.WebScrapingMain()
     
     #a weboldalakon keresi a tablazatokat
@@ -90,67 +92,95 @@ class WebScraping(object):
             element = self.list_end_cut_RON(element)
             element = ['RON'] + element
             self.List_elements += [element]
+        temp_List_elements.clear()
 
-    #  def json_file_upload(self):
-    #     list = []
-    #     index = 0
-    #     for i in range(len(self.List_elements)):
-    #         if i == 3 or i == 6:
-    #             index += 1
-    #         else:
-    #             pass
-    #         element = {
-    #             'web_address' : self.webpages_name[index],
-    #             'currency': self.List_elements[i][0],
-    #             'purchase_price':self.List_elements[i][1],
-    #             'sale_price': self.List_elements[i][2]
-    #         }
-    #         list += [element]
-    #     with open('..\\frontend\static\WebScraping.json','w') as json_file:
-    #          json.dump(list,json_file)
+    def EUROtoRON_GBP_USD(self):
+        value_pur = 0
+        value_sale = 0
+        index = 0
+        for element in self.List_elements:
+            if index %3 == 0:
+                euro_pur = self.List_elements[index][2]
+                euro_sale = self.List_elements[index][3]
+            if element[1] == 'EUR':
+                value_pur = element[2]
+                value_sale = element[3]
+                self.List_EUROtoRON_GBP_USD += [['EUR','RON',value_pur[:5],value_sale[:5]]]
+            else:
+                value_pur = str((1/float(element[2]))*float(euro_pur))
+                value_sale =str((1/float(element[3]))*float(euro_sale))
+                self.List_EUROtoRON_GBP_USD += [['EUR',element[1],value_pur[:5],value_sale[:5]]]
+            index += 1
+    
+    def GBPtoRON_EURO_USD(self):
+        value_pur = 0
+        value_sale = 0
+        index = 2
+        for element in self.List_elements:
+            if index == 2 or index == 5 or index == 8:
+                gbp_pur = self.List_elements[index][2]
+                gbp_sale = self.List_elements[index][3]
+            if element[1] == 'GBP':
+                value_pur = element[2]
+                value_sale = element[3]
+                self.List_GBPtoRON_EURO_USD += [['GBP','RON',value_pur[:5],value_sale[:5]]]
+            else:
+                value_pur = str((1/float(element[2]))*float(gbp_pur))
+                value_sale =str((1/float(element[3]))*float(gbp_sale))
+                self.List_GBPtoRON_EURO_USD += [['GBP',element[1],value_pur[:5],value_sale[:5]]]
+            index += 1
 
-    def json_file_upload(self):
+    def USDtoRON_EURO_GBP(self):
+        value_pur = 0
+        value_sale = 0
+        index = 1
+        for element in self.List_elements:
+            if index == 1 or index == 4 or index == 7:
+                usd_pur = self.List_elements[index][2]
+                usd_sale = self.List_elements[index][3]
+            if element[1] == 'USD':
+                value_pur = element[2]
+                value_sale = (element[3])
+                self.List_USDtoRON_EURO_GBP += [['USD','RON',value_pur[:5],value_sale[:5]]]
+            else:
+                value_pur = str((1/float(element[2]))*float(usd_pur))
+                value_sale =str((1/float(element[3]))*float(usd_sale))
+                self.List_USDtoRON_EURO_GBP += [['USD',element[1],value_pur[:5],value_sale[:5]]]
+            index += 1
+
+    def json_file_upload(self,file, json_list):
         list = []
-        list1 = []
-        list2 = []
-        a=0
-        b=3
-        for i in range(len(self.webpages_name)):
-            list2.clear()
-            for j in range(a,b):
-                # print (self.List_elements[j][1],self.List_elements[j][2],self.List_elements[j][3])
-                element2 = {
-                        'currency': self.List_elements[j][1],
-                        'purchase_price':self.List_elements[j][2],
-                        'sale_price': self.List_elements[j][3]
-                }
-                list2 += [element2]
-            element1 = {
-                'web_address' : self.webpages_name[i],
-                'values' : list2,
+        index = 0
+        for i in range(len(json_list)):
+            if i == 3 or i == 6:
+                index += 1
+            else:
+                pass
+            element = {
+                'web_address' : self.webpages_name[index],
+                'currency': json_list[i][1],
+                'purchase_price':json_list[i][2],
+                'sale_price': json_list[i][3]
             }
-            list1 += [element1]
-            print(element1)
-            a += 3
-            b += 3
-
-        # for i in list1:
-        #     print(i)
-
-        for valuta in range(len(self.List_valutas)):
-            if self.List_valutas[valuta] == self.List_elements[valuta][0]:
-                element = {
-                    'from': self.List_valutas[valuta],
-                    'web_page':list1
-                }
-                list += [element]
-        with open('..\\frontend\\static\\WebScraping.json','w') as json_file:
+            list += [element]
+        with open(file,'w') as json_file:
              json.dump(list,json_file)
+        list.clear()
         
     def WebScrapingMain(self):
         self.search_table()
         self.data_cleaning()
-        self.json_file_upload()
+        print(self.List_elements)
+        self.EUROtoRON_GBP_USD()
+        self.json_file_upload('..\\frontend\\static\\EURO.json',self.List_EUROtoRON_GBP_USD)
+        self.GBPtoRON_EURO_USD()
+        self.json_file_upload('..\\frontend\\static\\GBP.json',self.List_GBPtoRON_EURO_USD)
+        self.USDtoRON_EURO_GBP()
+        self.json_file_upload('..\\frontend\\static\\USD.json',self.List_USDtoRON_EURO_GBP)
+        self.List_elements.clear()
+        self.List_EUROtoRON_GBP_USD.clear()
+        self.List_GBPtoRON_EURO_USD.clear()
+        self.List_USDtoRON_EURO_GBP.clear()
 
 if __name__ == "__main__":
     webpages = []
